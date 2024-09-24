4b4eaea47fb831ec5d079963a3044b0fe721639d
StayLet <16182966+staylet@users.noreply.github.com>
2024-09-24 19:47:55 +0800

    feat: add unit test.

---
+++ src/pubmodule/pumodule_test.go
@@ -0,0 +1,37 @@
+/*
+A Test Go Public module.
+*/
+
+package pubmodule
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	cases := []struct {
+		want string
+	}{
+		{"Hello, Gopher!"},
+	}
+	for _, c := range cases {
+		got := Hello()
+		if got != c.want {
+			t.Errorf("Hello() = %#v, want %#v", got, c.want)
+		}
+	}
+}
+
+func TestGoodbye(t *testing.T) {
+	cases := []struct {
+		want string
+	}{
+		{"Goodbye, Gopher!"},
+	}
+	for _, c := range cases {
+		got := Goodbye()
+		if got != c.want {
+			t.Errorf("Goodbye() = %#v, want %#v", got, c.want)
+		}
+	}
+}
